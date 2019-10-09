3847e83cf3e5ed930658cf23402174a127b3fbff
Kelly Dwan <ryelle@users.noreply.github.com>
2019-10-09 14:31:24 -0400

    Use the post-type when choosing template partial

--- themes/wporg-5ftf/single.php
+++ themes/wporg-5ftf/single.php
@@ -9,7 +9,7 @@ get_header(); ?>
 		<?php while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/plugin', 'single' );
+			get_template_part( 'template-parts/content', get_post_type() );
 		endwhile; ?>
 
 	</main><!-- #main -->
