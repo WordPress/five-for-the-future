91ed1e933dd39a855de23cdf2424fe2d1879a144
Kelly Dwan <ryelle@users.noreply.github.com>
2019-10-09 14:31:41 -0400

    Use correct heading level for list pages

--- themes/wporg-5ftf/template-parts/content-page.php
+++ themes/wporg-5ftf/template-parts/content-page.php
@@ -8,7 +8,7 @@
 			<?php else : ?>
 
 				<a href="<?php the_permalink(); ?>">
-					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+					<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
 				</a>
 
 			<?php endif; ?>
