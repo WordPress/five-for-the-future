bc867a344e4a7b186644d75439153edc3f2db3ca
Kelly Dwan <ryelle@users.noreply.github.com>
2019-10-28 20:37:09 -0400

    Pledge List: Style search results

--- themes/wporg-5ftf/search.php
+++ themes/wporg-5ftf/search.php
@@ -19,12 +19,15 @@
 			while ( have_posts() ) :
 				the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+				get_template_part( 'template-parts/content', get_post_type() );
+
 			endwhile;
 
 			the_posts_pagination();
 
-		else :
+			?>
+
+		<?php else :
 
 			get_template_part( 'template-parts/content', 'none' );
 
