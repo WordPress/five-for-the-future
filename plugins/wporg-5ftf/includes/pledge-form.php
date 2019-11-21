c614d63056bd727fee1eeae545d60c3e20c147ef
Kelly Dwan <ryelle@users.noreply.github.com>
2019-11-21 16:42:59 -0500

    Fix issue with fetching data in `get_pledge_meta` when no submission has been POST'd

--- plugins/wporg-5ftf/includes/pledge-form.php
+++ plugins/wporg-5ftf/includes/pledge-form.php
@@ -213,7 +213,7 @@ function get_form_submission() {
 
 	$result = filter_input_array( INPUT_POST, $input_filters );
 	if ( ! $result ) {
-		return array_fill_keys( array_keys( $input_filters ), '' );
+		return [];
 	}
 
 	return $result;
