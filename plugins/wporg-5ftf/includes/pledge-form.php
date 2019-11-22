a947ec259d584a22403febc45170476fb01ac7ee
Kelly Dwan <ryelle@users.noreply.github.com>
2019-11-21 19:00:12 -0500

    Manage Pledge: Save a new logo if one is uploaded

--- plugins/wporg-5ftf/includes/pledge-form.php
+++ plugins/wporg-5ftf/includes/pledge-form.php
@@ -232,7 +232,20 @@ function process_form_manage( $pledge_id, $auth_token ) {
 
 	PledgeMeta\save_pledge_meta( $pledge_id, $submission );
 
-	// @todo Upload & attach logo.
+	if ( isset( $_FILES['org-logo'], $_FILES['org-logo']['tmp_name'] ) && ! empty( $_FILES['org-logo']['tmp_name'] ) ) {
+		$logo_attachment_id = upload_image( $_FILES['org-logo'] );
+		if ( is_wp_error( $logo_attachment_id ) ) {
+			return $logo_attachment_id;
+		}
+
+		// Attach logo to the pledge.
+		wp_update_post( array(
+			'ID'          => $logo_attachment_id,
+			'post_parent' => $pledge_id,
+		) );
+		set_post_thumbnail( $pledge_id, $logo_attachment_id );
+	}
+
 	// @todo Save contributors.
 
 	// If we made it to here, we've successfully saved the pledge.
