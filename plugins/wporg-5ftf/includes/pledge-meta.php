725fdbb94beb49a777da1995a3a9c40eb6170397
Kelly Dwan <ryelle@users.noreply.github.com>
2019-11-22 16:10:52 -0500

    Hide confirmation when pledge is a draft

--- plugins/wporg-5ftf/includes/pledge-meta.php
+++ plugins/wporg-5ftf/includes/pledge-meta.php
@@ -209,6 +209,7 @@ function add_meta_boxes() {
 function render_meta_boxes( $pledge, $box ) {
 	$readonly  = ! current_user_can( 'edit_page', $pledge->ID );
 	$is_manage = true;
+	$pledge_id = $pledge->ID;
 
 	$data = array();
 	foreach ( get_pledge_meta_config() as $key => $config ) {
