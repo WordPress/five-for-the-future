913461cc4a7807f3b7a609ae1a2b949921a3ccc3
Ian Dunn <ian@iandunn.name>
2022-08-18 08:12:40 -0700

    Tests: Modularize database fixtures for reuse.

--- plugins/wporg-5ftf/tests/bootstrap.php
+++ plugins/wporg-5ftf/tests/bootstrap.php
@@ -22,6 +22,7 @@ require_once $_tests_dir . '/includes/functions.php';
  */
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/index.php';
+	require __DIR__ . '/helpers.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
